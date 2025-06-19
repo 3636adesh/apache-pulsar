dda3d99b957898ccc193f70c9eeb473d0f3ca10b
adeshmalunjkar <adesh.malunjkar@upgrad.com>
2025-06-19 15:55:48 +0530

    add .gitignore file to exclude IDE and build artifacts

---
+++ src/main/java/com/malunjkar/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.malunjkar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://github.com/majusko/pulsar-java-spring-boot-starter
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
